319c713e09063fcf78e5151382d207d44c5b78ca
Jakub Okoński <kuba@okonski.org>
2011-03-27 11:14:37 +0200

    Initial scaffold.

---
+++ Gemfile
@@ -0,0 +1,3 @@
+gem 'qtbindings', :require => 'Qt4'
+gem 'activerecord'
+gem 'sqlite3-ruby', :require => 'sqlite3'
---
+++ manager.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+require 'bundler'
+Bundler.setup
+
+puts "Initialized."
+
+require 'Qt4'
+
+app = Qt::Application.new(ARGV)
+
+quit = Qt::PushButton.new('Quit')
+quit.resize(75, 30)
+quit.setFont(Qt::Font.new('Times', 18, Qt::Font::Bold))
+
+Qt::Object.connect(quit, SIGNAL('clicked()'), app, SLOT('quit()'))
+
+quit.show()
+app.exec()
